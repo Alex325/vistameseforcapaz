2b16473396e6226d6dfce3174ff945021815b869
Alex Sandro <alexsandromacedopinto@gmail.com>
2023-12-21 11:31:26 -0300

    Sim.

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import Slider from '@react-native-community/slider';
+import MaskedView from '@react-native-masked-view/masked-view';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import ColorPicker from 'react-native-wheel-color-picker';
 
 export default function App() {
+  const [color, setColor] = useState('');
+
+  const onColorChange = color => {
+    setColor(color);
+  };
   return (
     <View>
-      <Text>Amogus</Text>
-      <Slider step={1} minimumValue={0} maximumValue={4} />
+      <ColorPicker
+        color={color}
+        onColorChange={onColorChange}
+        thumbSize={30}
+        sliderSize={30}
+        noSnap={true}
+      />
+      <View style={{ backgroundColor: '#000000', }}>
+        <Image style={{}} source={require('./assets/sim.png')} />
+      </View>
     </View>
   );
 }
